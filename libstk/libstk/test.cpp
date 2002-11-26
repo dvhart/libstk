24069f98ec303a9014d50a27e83479e5e8fa0fdd
Darren Hart <darren@dvhart.com>
2002-11-26 23:26:18 +0000

    Initial thoughts on boost signals, see widget.h

--- libstk/libstk/test.cpp
+++ libstk/libstk/test.cpp
@@ -5,14 +5,18 @@
 #include <iostream>
 #include <typeinfo>
 
+
+typedef boost::shared_ptr<stk::app>    App;
+typedef boost::shared_ptr<stk::state>  State;
+typedef boost::shared_ptr<stk::widget> Widget;
+
 int main(int argc,char* arv[])
 {
-	boost::shared_ptr<stk::app> testapp(new stk::app);
-	boost::shared_ptr<stk::state> teststate(new stk::state(testapp));
+	App test_app(new stk::app);  // (new stk::app) as an argument is weird
+	State test_state(new stk::state(testapp));
 	testapp->current_state(teststate);
 	
-	boost::shared_ptr<stk::widget> a(new stk::widget(teststate)) ,b(new stk::widget(teststate));
+	Widget a(new stk::widget(teststate)) ,b(new stk::widget(teststate));
 	teststate->add(a);
 	teststate->add(b);
-	
 }
--- libstk/libstk/widget.h
+++ libstk/libstk/widget.h
@@ -21,6 +21,7 @@
 #include <string>
 #include <boost/shared_ptr.hpp>
 #include <boost/weak_ptr.hpp>
+#include <boost/signal.hpp>
 #include <SDL/SDL.h>
 
 namespace stk
@@ -38,13 +39,56 @@ public:
 	widget(boost::weak_ptr<container> parent);
 	~widget();
 
-    /* DEMO , not really implemented (CHANGE TO BOOST::SIGNALS!)
-    SigC::Signal1<void, widget&>         on_focus;     // void sig(widget&)
-    SigC::Signal1<void, widget&>         on_unfocus;   // void sig(widget&)
-	SigC::Signal2<bool, widget&, SDLKey> on_keydown;  // bool sig(widget&, SDLKey)
-	SigC::Signal2<bool, widget&, SDLKey> on_keyup;  // bool sig(widget&, SDLKey)
-    SigC::Signal2<bool, widget&, SDLKey> on_keypress;  // bool sig(widget&, SDLKey)
-	*/
+	/*
+	 The signals currently all return void, however, it may be useful
+	 to define an AND combiner functor and have all slots return bool.  
+	 Then if any of the slots fail, the signal returns false, otherwise
+	 it returns true.  We might do something like:
+
+	 struct sig_and 
+	 { 
+	   bool operator ()(InputIterator first, InputIterator last) const
+		 {
+		   if (first == last) return true; // no connections, so we didn't fail
+			 while (first != last) {
+         if (! *first) return false; // one failed, so return false
+         ++first;
+			 }
+			 return true; // no connections failed
+		 }
+	 }
+	 
+	 boost::signal<bool (), sig_and> sig;
+
+	 we can of course let the "user" specify all this... but as a widget set,
+	 I think it makes since to define a standard interface, and I doubt
+	 users are going to want to receive a vector of return values...
+
+	 sorry for the spewage in Marc's beautiful code... :-)
+	 
+	 */
+	boost::signal<void ()> on_focus;
+	boost::signal<void ()> on_unfocus;
+	boost::signal<void (SDLKey)> on_keydown;
+	boost::signal<void (SDLKey)> on_keyup;
+
+	/*
+	 //some cool things we can do with boost::signals
+	 
+	 VideoPanel my_vp(new stk::video_panel);
+	 Button my_button(new stk::button);
+	 
+	 // we don't need any arguments in general for slots since we can connect
+	 // functors, and specific object member variables
+	 my_button.on_click.connect(boost::bind(&stk::video_panel::play, my_vp));
+
+	 // perhaps we want to rewind and start over on_click (in that order)
+	 my_button.on_click.connect(0, boost::bind(&stk::video_panel::rewind, my_vp));
+	 my_button.on_click.connect(1, boost::bind(&stk::video_panel::play, my_vp));
+	 
+	 */
+
+	
 protected:
     boost::weak_ptr<container> parent_;
 };
