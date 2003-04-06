3bb3c033c66b2ce9000c68f8cabe12a2335ec3dd
Mark Stramke <marcstraemke@gmx.net>
2003-04-06 12:09:36 +0000

    Commmited basic documentation, mainly serves as a starting point for the rest of the team to start writing docs

--- libstk/doc/process_xml.sh
+++ libstk/doc/process_xml.sh
@@ -3,6 +3,7 @@ xmllint --noout --postvalid --loaddtd libstk.xml
 SUCCESS=$?
 if [ "$SUCCESS" = "0" ]; then
     xsltproc -o output/ htmlchunkedsheet.xsl libstk.xml;
+    dot dot_files/widget_tree_example.dot -o images/widget_tree_example.png -Tpng
 else
     echo Libstk.xml didnt validate against docbook dtd;
 fi
