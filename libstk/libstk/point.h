874722805aeb37312094d6dc38505f244778b2bd
Mark Stramke <marcstraemke@gmx.net>
2003-05-03 11:54:15 +0000

    Modified Point class to contain additional Overloaded operators to make it more useful

--- libstk/libstk/point.h
+++ libstk/libstk/point.h
@@ -1,9 +1,13 @@
 #ifndef STK_POINT_H
 #define STK_POINT_H
 
+#include <boost/operators.hpp>		
+
 namespace stk
 {
-	class point
+	class point    
+			: boost::addable< point> 
+    		, boost::subtractable<point>
 	{
 		private:
 			int x_, y_;
@@ -13,6 +17,18 @@ namespace stk
 			int y() const { return y_; }
 			void x(int x) { x_ = x; }
 			void y(int y) { y_ = y; }
+		
+			point operator+=(const point& rhs)	
+				{
+					x_+=rhs.x_;
+					y_+=rhs.y_;
+				}
+    		point operator-=(const point& rhs)
+				{
+					x_-=rhs.x_;
+					y_-=rhs.y_;
+				}
+    		
 	};
 }
 #endif
