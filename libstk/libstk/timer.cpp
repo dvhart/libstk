f259b817a039402142208ecf3d083c7c6d9d2600
Darren Hart <darren@dvhart.com>
2003-05-20 20:47:00 +0000

    TODO update, timer comment

--- libstk/libstk/timer.cpp
+++ libstk/libstk/timer.cpp
@@ -2,7 +2,7 @@
  *    FILENAME: timer.cpp
  * DESCRIPTION: Timer class implementation.
  *     AUTHORS: Darren Hart
- *  START DATE: 13/May/2003  LAST UPDATE: 14/May/2003
+ *  START DATE: 13/May/2003  LAST UPDATE: 20/May/2003
  *
  *   COPYRIGHT: 2003 by Darren Hart, Vernon Mauery, Marc Straemke, Dirk Hoerner
  *     LICENSE: This software is licenced under the Libstk license available
