89835cea6031179d89d5b0b844123e2a57b841c9
Darren Hart <darren@dvhart.com>
2003-08-06 19:29:52 +0000

    new header test... again

--- libstk/libstk/widget.h
+++ libstk/libstk/widget.h
@@ -1,8 +1,8 @@
 /**************************************************************************************************
- *    FILENAME: $RCSfile$
+ *     CVS $Id$
  * DESCRIPTION: The abstract base class for all widgets.
  *     AUTHORS: Darren Hart
- *  START DATE: 2002/Sep/08  LAST UPDATE: $Date$ by $Author$
+ *  START DATE: p/08/2/S/20
  *
  *   COPYRIGHT: 2003 by Darren Hart, Vernon Mauery, Marc Straemke, Dirk Hoerner
  *     LICENSE: This software is licenced under the Libstk license available with the source as 
