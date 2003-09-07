6fa4c92ef753d2f9c070ff1d5b04ada5c4039378
Mark Stramke <marcstraemke@gmx.net>
2003-09-07 15:43:47 +0000

    Slightly Improved Doxygen's Output by including Dummy Templates for Common STL vector templates

---
+++ libstk/doc/doxygen_dummies.h
@@ -0,0 +1,12 @@
+namespace std
+{
+    typedef <typename T>
+    class vector
+    {};
+    typedef <typename T>
+    class list
+    {};
+    typedef <typename T, typename T2>
+    class map
+    {};
+}
