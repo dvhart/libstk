01680e37eac2b1d78de89aa6af793181443b6b84
Mark Stramke <marcstraemke@gmx.net>
2003-10-09 13:24:48 +0000

    made backend selection dynamic in test_area (worked with sdl only before)

--- libstk/test/test_area.cpp
+++ libstk/test/test_area.cpp
@@ -12,8 +12,20 @@
 #include <iostream>
 #include <libstk/stk.h>
 
+// backend specific includes (we select it dynamically, so include them all if built)
+#ifdef HAVE_SDL
 #include <libstk/event_producer_sdl.h>
 #include <libstk/surface_sdl.h>
+#endif
+#ifdef HAVE_DIRECTFB
+#include <libstk/surface_dfb.h>
+// what would be a good default backend ?
+#endif
+#ifdef HAVE_FBDEV
+#include <libstk/surface_fbdev.h>
+// what would be a good default backend ?
+#endif
+// FIXME: should this be configurable also ?  --enable-elotouch ?
 #include <libstk/event_producer_elotouch.h>
 
 using namespace stk;
@@ -24,17 +36,66 @@ int main(int argc, char* argv[])
 
     try
     {
+#ifdef HAVE_LOGGING
         logger::get()->add_target(&std::cout, LL_Info);
+#endif
+        std::string surface_type;
+        std::string available_surfaces = "[ ";
 
-        // select the surface and event system
-        surface::ptr screen;
+	// select the surface and event system
+#ifdef HAVE_SDL
+        available_surfaces += "sdl |";
+#endif
+#ifdef HAVE_DIRECTFB
+        available_surfaces += " dfb |";
+#endif
+#ifdef HAVE_FBDEV
+        available_surfaces += " fbdev";
+#endif
+        available_surfaces += " ]";
+	
+        if (argc < 2)
+        {
+            throw error_message_exception("Usage: hello_world "+available_surfaces);
+        }
+        else
+        {
+            surface_type = std::string(argv[1]);
+        }
+
+	surface::ptr screen;
         event_producer::ptr ep;
-        INFO("creating sdl surface");
-        screen = surface_sdl::create(rectangle(0, 0, 640, 480), false);
-        INFO("creating sdl event_producer");
-        ep = event_producer_sdl::create();
 
-        
+	if (0) { }
+#ifdef HAVE_SDL
+        else if (surface_type == "sdl")
+        {
+            INFO("creating sdl surface");
+            screen = surface_sdl::create(rectangle(0, 0, 640, 480), false); // not full screen
+            INFO("creating sdl event_producer");
+            ep = event_producer_sdl::create();
+        }
+#endif
+#ifdef HAVE_DIRECTFB
+        else if (surface_type == "dfb")
+        {
+            screen = surface_dfb::create(rectangle(0, 0, 640, 480));
+            // FIXME: use something a bit more generic for the test app
+            ep = event_producer_elotouch::create("/dev/tts/0");
+        }
+#endif
+#ifdef HAVE_FBDEV
+        else if (surface_type == "fbdev")
+        {
+            screen = surface_fbdev::create(rectangle(0, 0, 1024, 768));
+#ifdef HAVE_SDL // needed for the event_system
+            ep = event_producer_sdl::create();
+#endif
+        }
+#endif
+        else
+            throw error_message_exception("Unknown surface type");
+	
         // create the application
         application::ptr app = application::create(screen);
 
