83a3e7c7f4b556366d117b75ed6599355c4a6d94
Darren Hart <darren@dvhart.com>
2005-04-19 19:10:20 +0000

    changed configure.in to test for libboost_signals and libboost_threads, rather than specific versions of those libs

--- libstk/configure_all.sh
+++ libstk/configure_all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
-./configure --enable-sdl --enable-directfb --enable-logging \
-            --enable-xine --enable-tests
+#./configure --enable-sdl --enable-directfb --enable-logging \
+#            --enable-xine --enable-tests
 
+./configure --enable-sdl --enable-logging --enable-tests
